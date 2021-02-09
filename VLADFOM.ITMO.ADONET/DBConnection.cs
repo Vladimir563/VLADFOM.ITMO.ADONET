bf8d1f999e668625570aabe80f523dbab1b94a70
Vladimir Fominykh <vladimir3591f@mail.ru>
2021-02-09 15:45:44 +0300

    TDP1# DBConnection form added

---
+++ VLADFOM.ITMO.ADONET/DBConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace VLADFOM.ITMO.ADONET
+{
+    public partial class DBConnection : Form
+    {
+        SqlConnection connection = new SqlConnection();
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind; Integrated Security=true";
+        public DBConnection()
+        {
+            InitializeComponent();
+        }
+
+        private void connectToDBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try 
+            { 
+                if (connection.State != ConnectionState.Open) 
+                { 
+                    connection.ConnectionString = connectionString; connection.Open(); 
+                    MessageBox.Show("Соединение с базой данных " + connection.Database + " выполнено успешно " + "\nСервер: " + connection.DataSource); 
+                } 
+                
+                else MessageBox.Show("Соединение с базой данных уже установлено"); }
+            catch
+            { 
+                MessageBox.Show("Ошибка соединения с базой данных"); }
+            }
+
+        private void disconnectFromDBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close(); MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else MessageBox.Show("Соединение с базой данных уже закрыто");
+        }
+    }
+}
--- VLADFOM.ITMO.ADONET/Program.cs
+++ VLADFOM.ITMO.ADONET/Program.cs
@@ -16,7 +16,7 @@ namespace VLADFOM.ITMO.ADONET
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new DBConnection());
         }
     }
 }
