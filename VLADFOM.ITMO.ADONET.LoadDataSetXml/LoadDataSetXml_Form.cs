83c516fcfaf92c065ed91e62d982cc704c833b26
Vladimir Fominykh <vladimir3591f@mail.ru>
2021-02-19 21:19:41 +0300

    TDP#10 Exercise 6 all parts solved and added

---
+++ VLADFOM.ITMO.ADONET.LoadDataSetXml/LoadDataSetXml_Form.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VLADFOM.ITMO.ADONET.LoadDataSetXml
+{
+    public partial class LoadDataSetXml_Form : Form
+    {
+        DataSet NorthwindDataSet = new DataSet();
+        public LoadDataSetXml_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadSchemaButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet.ReadXmlSchema("Northwind.xsd");
+
+            CustomersGrid.DataSource = NorthwindDataSet.Tables["Customers"];
+            OrdersGrid.DataSource = NorthwindDataSet.Tables["Orders"];
+        }
+
+        private void LoadDataButton_Click(object sender, EventArgs e)
+        {
+            NorthwindDataSet.ReadXml("Northwind.xml");
+        }
+    }
+}
---
+++ VLADFOM.ITMO.ADONET.SavingDataSetXml/SavingDataSetXml_Form.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VLADFOM.ITMO.ADONET.SavingDataSetXml
+{
+    public partial class SavingDataSetXml_Form : Form
+    {
+        public SavingDataSetXml_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void FillDataSetButton_Click(object sender, EventArgs e)
+        {
+            CustomersAdapter.Fill(northwindDataSet1.Customers); 
+            OrdersAdapter.Fill(northwindDataSet1.Orders);
+
+            CustomersGrid.DataSource = northwindDataSet1.Customers;
+        }
+
+        private void SaveXmlDataButton_Click(object sender, EventArgs e)
+        {
+            try 
+            { 
+                northwindDataSet1.WriteXml("Northwind.xml"); 
+                MessageBox.Show("Data save as XML"); } 
+            catch (Exception ex) 
+            { 
+                MessageBox.Show(ex.Message); 
+            }
+        }
+
+        private void SaveXmlSchemaButton_Click(object sender, EventArgs e)
+        {
+            try 
+            { 
+                northwindDataSet1.WriteXmlSchema("Northwind.xsd"); 
+                MessageBox.Show("Schema save as XML"); 
+            } 
+            catch (Exception ex) 
+            { 
+                MessageBox.Show(ex.Message); 
+            }
+        }
+    }
+}
