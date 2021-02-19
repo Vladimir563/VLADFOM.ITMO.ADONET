e9682a798e43cbd61797a415144498087059edb5
Vladimir Fominykh <vladimir3591f@mail.ru>
2021-02-19 22:29:22 +0300

    TDP#12 Exercise 8 all parts solved and added

---
+++ VLADFOM.ITMO.ADONET.LINQsql_m/LINQsql_m_Form.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VLADFOM.ITMO.ADONET.LINQsql_m
+{
+    public partial class LINQsql_m_Form : Form
+    {
+        public LINQsql_m_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var db = new DataClassesProcDataContext();
+            foreach (var r in db.Ten_Most_Expensive_Products())
+            {
+                ListViewItem item =
+                listView1.Items.Add(r.TenMostExpensiveProducts.ToString());
+                item.SubItems.Add(r.UnitPrice.ToString());
+            }
+        }
+    }
+}
