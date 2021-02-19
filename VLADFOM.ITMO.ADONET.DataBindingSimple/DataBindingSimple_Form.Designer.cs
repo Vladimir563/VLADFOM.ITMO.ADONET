87bc27a0812bbec1876ff4df07da3ff48e1c92cb
Vladimir Fominykh <vladimir3591f@mail.ru>
2021-02-19 20:53:21 +0300

    TDP#9 Exercise 5 all parts solved and added

---
+++ VLADFOM.ITMO.ADONET.DataBindingSimple/DataBindingSimple_Form.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace VLADFOM.ITMO.ADONET.DataBindingSimple
+{
+    partial class DataBindingSimple_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.northwindDataSet1 = new VLADFOM.ITMO.ADONET.DataBindingSimple.NorthwindDataSet();
+            this.productsTableAdapter1 = new VLADFOM.ITMO.ADONET.DataBindingSimple.NorthwindDataSetTableAdapters.ProductsTableAdapter();
+            this.ProductIDTextBox = new System.Windows.Forms.TextBox();
+            this.ProductNameTextBox = new System.Windows.Forms.TextBox();
+            this.PreviousButton = new System.Windows.Forms.Button();
+            this.NextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // northwindDataSet1
+            // 
+            this.northwindDataSet1.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter1
+            // 
+            this.productsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // ProductIDTextBox
+            // 
+            this.ProductIDTextBox.Location = new System.Drawing.Point(13, 13);
+            this.ProductIDTextBox.Name = "ProductIDTextBox";
+            this.ProductIDTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ProductIDTextBox.TabIndex = 0;
+            // 
+            // ProductNameTextBox
+            // 
+            this.ProductNameTextBox.Location = new System.Drawing.Point(119, 13);
+            this.ProductNameTextBox.Name = "ProductNameTextBox";
+            this.ProductNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ProductNameTextBox.TabIndex = 1;
+            // 
+            // PreviousButton
+            // 
+            this.PreviousButton.Location = new System.Drawing.Point(13, 40);
+            this.PreviousButton.Name = "PreviousButton";
+            this.PreviousButton.Size = new System.Drawing.Size(100, 23);
+            this.PreviousButton.TabIndex = 2;
+            this.PreviousButton.Text = "Previous";
+            this.PreviousButton.UseVisualStyleBackColor = true;
+            this.PreviousButton.Click += new System.EventHandler(this.PreviousButton_Click);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Location = new System.Drawing.Point(119, 40);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(100, 23);
+            this.NextButton.TabIndex = 3;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = true;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // DataBindingSimple_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 81);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.PreviousButton);
+            this.Controls.Add(this.ProductNameTextBox);
+            this.Controls.Add(this.ProductIDTextBox);
+            this.Name = "DataBindingSimple_Form";
+            this.Text = "DataBindingSimple_Form";
+            this.Load += new System.EventHandler(this.DataBindingSimple_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NorthwindDataSet northwindDataSet1;
+        private NorthwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter1;
+        private System.Windows.Forms.TextBox ProductIDTextBox;
+        private System.Windows.Forms.TextBox ProductNameTextBox;
+        private System.Windows.Forms.Button PreviousButton;
+        private System.Windows.Forms.Button NextButton;
+    }
+}
+
---
+++ VLADFOM.ITMO.ADONET.DataBindingSimple/DataBindingSimple_Form.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VLADFOM.ITMO.ADONET.DataBindingSimple
+{
+    public partial class DataBindingSimple_Form : Form
+    {
+        private BindingSource productsBindingSource;
+        public DataBindingSimple_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void DataBindingSimple_Form_Load(object sender, EventArgs e)
+        {
+            productsTableAdapter1.Fill(northwindDataSet1.Products);
+
+            productsBindingSource = new BindingSource(northwindDataSet1, "Products");
+
+            ProductIDTextBox.DataBindings.Add("Text", productsBindingSource, "ProductID");
+            ProductNameTextBox.DataBindings.Add("Text", productsBindingSource, "ProductName");
+        }
+
+        private void PreviousButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MovePrevious();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            productsBindingSource.MoveNext();
+        }
+    }
+}
