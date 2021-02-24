7c5c70cda8d7abd438f82bac4632cf15f0ae6197
Vladimir Fominykh <vladimir3591f@mail.ru>
2021-02-24 20:34:50 +0300

    TDP#13 Exercise 9 all parts solved and added

---
+++ VLADFOM.ITMO.ADONET.CodeFirst/SampleContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace VLADFOM.ITMO.ADONET.CodeFirst
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("MyShop")
+        { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        //public DbSet<VipOrder> VipOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+            .Property(c => c.LastName).IsRequired().HasMaxLength(30);
+        }
+    }
+}
